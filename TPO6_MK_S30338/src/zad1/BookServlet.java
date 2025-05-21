0777fa5f694d235da1c0e4e1beb1384c2392eee4
StrangeDuckk <mikusek.kasia@gmail.com>
2025-05-21 15:33:41 +0200

    updated gitignore and initial commit

---
+++ TPO6_MK_S30338/src/zad1/BookServlet.java
@@ -0,0 +1,23 @@
+package zad1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class BookServlet extends HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        /*
+        Aplikacja Web
+
+        Napisać aplikację WEB pozwalającą na przeglądanie i wyszukiwanie książek w bazie danych książek.
+        Baza książek - z wykładu o JDBC z  przedmiotu UTP  (koniecznie użyć bazy Derby).
+        Technologie - z wykładu o Aplikacjach WEB.
+         */
+
+        //todo uzywac javax.servlet -> logika + wyglad
+        //java server pages -> wyglad
+        // java server faces -> elastyczne generowanie gui
+
+    }
+}
